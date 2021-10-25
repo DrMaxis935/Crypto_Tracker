48ba356abc188734582d7089d87c6fc6d35751ba
Dr Maxis <axelrousseau935@gmail.com>
2021-10-25 23:21:30 +0200

    affichage des currency

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DashboardController extends AbstractController
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @Route("/dashboard")
+     */
+    public function dashboard(): Response
+    {
+        $api_url = $this->getParameter('coinmarketcap.api.url');
+        $token = $this->getParameter('coinmarketcap.api.token');
+
+        $response = $this->client->request(
+            'GET',
+            $api_url,
+            [
+                'query' => [
+                    'start' => 1,
+                    'limit' => 5,
+                    'convert' => 'USD'
+                ],
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'X-CMC_PRO_API_KEY' => $token
+                ]
+            ]
+        );
+
+        $currencies = $response->toArray()['data'];
+
+        return $this->render('dashboard.html.twig', [
+            'api_url' => $api_url,
+            'currencies' => $currencies
+        ]);
+    }
+}
